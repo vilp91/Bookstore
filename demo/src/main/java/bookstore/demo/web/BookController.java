7d99866407999b7f8290d1f7a45e51f5c16e0122
vilp91 <vpajukangas@gmail.com>
2024-02-26 23:27:15 +0200

    Delete and add

--- demo/src/main/java/bookstore/demo/web/BookController.java
+++ demo/src/main/java/bookstore/demo/web/BookController.java
@@ -3,12 +3,13 @@ package bookstore.demo.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import bookstore.demo.domain.Book;
 import bookstore.demo.domain.BookRepository;
+
+
 
 
 
@@ -18,12 +19,27 @@ public class BookController {
     public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
-
     @GetMapping("/booklist")
     public String getBooks(Model model) {
         Iterable<Book> books = bookRepository.findAll();
         model.addAttribute("books", books);
         return "booklist";
     }
-    
+    @GetMapping("/add")
+    public String showbookform(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+    @SuppressWarnings("null")
+    @PostMapping("/add")
+    public String addbook(@ModelAttribute Book newBook) {
+        bookRepository.save(newBook);
+        return "redirect:/booklist";
+    }
+    @SuppressWarnings("null")
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
+    }
 }
