d1f056a7b4e39c71c21da6a7fc05be5558ec727a
vilp91 <vpajukangas@gmail.com>
2024-03-25 16:00:53 +0200

    securing and login/logout functionality

--- demo/src/main/java/bookstore/demo/web/BookController.java
+++ demo/src/main/java/bookstore/demo/web/BookController.java
@@ -1,6 +1,6 @@
 package bookstore.demo.web;
 
-//import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,7 +40,7 @@ public class BookController {
     }
     @SuppressWarnings("null")
     @GetMapping("/delete/{id}")
-    //@PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public String deleteBook(@PathVariable Long id) {
         bookRepository.deleteById(id);
         return "redirect:/booklist";
